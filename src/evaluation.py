97b0980067bd49820a4c347859c7f70c41214d01
nick7948 <sanchit.8794@gmail.com>
2024-11-07 21:30:04 +0000

    Initial commit of Volatility Prediction project

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_squared_error
 import os
 
 def evaluate_model(model, X_test, y_test, dir="results/figures"):
     y_pred = model.predict(X_test)
     mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    rmse = mean_squared_error(y_test, y_pred, squared=False) 
     print("Test MSE:", mse)
 
     os.makedirs(dir, exist_ok=True)
